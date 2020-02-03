f6022b88299a84de453493df2ff37d6834973ab3
guccixversace <2098064@talnet.nl>
2020-02-03 09:35:29 +0100

    .
    .

---
+++ javascript.js
@@ -0,0 +1,17 @@
+var a = 10;
+if(a > 9) {
+    console.log("a is bigger than 9")
+}
+
+for (var i=0; i< 10; i++) {
+    console.log("i is now" + i)
+}
+
+var mycar = {
+    brand: "Ferrari",
+    type: "Spider",
+    amountofWheels: 4,
+    color: "red",
+}
+
+console.log(mycar.color);
